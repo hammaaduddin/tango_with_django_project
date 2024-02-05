8e56968a97032e19bbfe5c859f6330d189c40164
Hammaad Uddin <2787006U@student.gla.ac.uk>
2024-02-05 01:58:26 +0000

    update

--- rango/urls.py
+++ rango/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('category/<slug:category_name_slug>/',
           views.show_category, name='show_category'),
-    path('page/<slug:page_name_slug>/',
-          views.show_page, name='show_page')
+    path('add_category/', views.add_category, name='add_category'),
 
 ]
